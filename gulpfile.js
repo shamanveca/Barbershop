6020e905339d1b2a2461bdc0db482dd9e33f760e
ShamanVeca <shaman98@list.ru>
2025-09-12 18:47:03 +0300

    Добавляет новые модули для оптимизации и прописывает новый код в gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,119 @@
 //     .pipe(gulp.dest('./css'));
 // });
 
-"use strict";
+// ------------------------------------------------------------
 
-var gulp = require("gulp");
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+// "use strict";
 
-gulp.task("style", function() {
-    gulp.src("less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-        autoprefixer()
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
-});
+// var gulp = require("gulp");
+// var less = require("gulp-less");
+// var plumber = require("gulp-plumber");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var serverr = require("browser-sync").create();
 
-gulp.task("serve", ["style"], function() {
-    server.init({
-        server: ".",
-        notify: false,
-        open: true,
-        cors: true,
-        ui: false
-    });
+// gulp.task("style", async function() {
+//     gulp.src("./less/style.less")
+//     .pipe(plumber())
+//     .pipe(less())
+//     .pipe(postcss([
+//         autoprefixer()
+//     ]))
+//     .pipe(gulp.dest("css"))
+//     .pipe(server.stream())
+// });
 
-    gulp.watch("less/**/*.less", ["style"]);
-    gulp.watch("*.html").on("change", server.reload);
-});
+// gulp.task("serve", gulp.parallel("style"), function() {
+//     serverr.init({
+//         // server: "./",
+//         // notify: false,
+//         // open: true,
+//         // cors: true,
+//         // ui: false
+
+//         server: {
+//             baseDir: "../BarbershopAll/"
+//         }
+//     });
+
+    
+
+//     gulp.watch("./less/**/*.less", "style")
+//     gulp.watch("./*.html").on("change", serverr.reload)
+// });
+
+// ----------------------------------------------------------
+
+
+
+const gulp = require('gulp');
+const less = require('gulp-less'); // Преобразует less в css
+const del = require('del'); // Удаляет файлы
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css'); // Минифицирует CSS
+
+// Используется для преодбразования js 
+// в универсальный js для старых версий браузеров
+const babel = require('gulp-babel');
+
+const uglify = require('gulp-uglify'); // Минифицирует JS
+const concat = require('gulp-concat'); // Для объединения нескольких файлов в один
+
+
+// Пути к файлам откуда и куда
+const paths = {
+    styles: {
+        src: './less/**/*.less',
+        dest: './css'
+    },
+
+    scripts: {
+        src: './js/**/*.js',
+        dest: './js-min'
+    }
+}
+
+function clean() {
+    return del(['./css/style.min.css']) // Не работает :(
+}
+
+// Функция для преобразования less в css
+function styles() {
+    return gulp.src(paths.styles.src)
+     .pipe(less())
+     .pipe(cleanCSS()) // Оптимизирует и минифицирует код
+     .pipe(rename({     // переименовывает файл и добавляет ему суффикс
+        basename: 'style',
+        suffix: '.min'
+     }))
+
+     .pipe(gulp.dest(paths.styles.dest))
+}
+
+// Функция для преобразования js
+function scripts() {
+    return gulp.src(paths.scripts.src, {
+        sourcemaps: true
+    })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('index.min.js')) // Объединяем все файлы в один с указанным названием
+
+    .pipe(gulp.dest(paths.scripts.dest))
+}
+
+
+// Функция отслеживания действий
+function watch() {
+    gulp.watch(paths.styles.src, styles)
+    gulp.watch(paths.scripts.src, scripts)
+}
+
+const build = gulp.series(gulp.parallel(styles, scripts), watch);
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
+exports.default = build; // Вызыввет build по одной команде gulp
